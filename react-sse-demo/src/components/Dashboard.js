970d27a5fad292835c74b6bd6f747f45c88d1254
DESKTOP-N0GACVK\User <seunghoon@ontune.co.kr>
2022-11-11 16:56:04 +0900

    test

--- react-sse-demo/src/components/Dashboard.js
+++ react-sse-demo/src/components/Dashboard.js
@@ -1,142 +1,113 @@
+import { TextArea } from 'devextreme-react';
 import React, { useEffect, useState } from 'react'
 import ReactSpeedometer from "react-d3-speedometer"
 
 
 export const Dashboard = () => {
 
-    const [cpuUsagePs, setcpuUsagePs] = useState(0);
-    const [memoryUsagePs, setmemoryUsagePs] = useState(0);
-    const [currentDatePs, setcurrentDatePs] = useState(0);
-    const [taskTimePs, settaskTimePs] = useState(0);
+    // const [cpuUsagePs, setcpuUsagePs] = useState(0);
+    // const [memoryUsagePs, setmemoryUsagePs] = useState(0);
+    // const [currentDatePs, setcurrentDatePs] = useState(0);
+    // const [taskTimePs, settaskTimePs] = useState(0);
 
-    const [cpuUsageTs, setcpuUsageTs] = useState(0);
-    const [memoryUsageTs, setmemoryUsageTs] = useState(0);
-    const [currentDateTs, setcurrentDateTs] = useState(0);
-    const [taskTimeTs, settaskTimeTs] = useState(0);
+    // const [cpuUsageTs, setcpuUsageTs] = useState(0);
+    // const [memoryUsageTs, setmemoryUsageTs] = useState(0);
+    // const [currentDateTs, setcurrentDateTs] = useState(0);
+    // const [taskTimeTs, settaskTimeTs] = useState(0);
 
-    const [cpuUsageEs, setcpuUsageEs] = useState(0);
-    const [memoryUsageEs, setmemoryUsageEs] = useState(0);
-    const [currentDateEs, setcurrentDateEs] = useState(0);
-    const [taskTimeEs, settaskTimeEs] = useState(0);
+    
+    const [agentData, setAgentData] = useState(0);
+    const [realTimeData, setRealTimeData] = useState(0);
 
-    const [data, setData] = useState(0);
-
-    let postgreInterval;
-    let tsdbInterval;
-    let elasticInterval;
+    let realTimeInterval;
+    let agentInfoInterval;
+    
 
 
     let eventMemory1 = undefined;
     let eventMemory2 = undefined;
-    let eventMemory3 = undefined;
+    
 
   
     useEffect(() => {
-        function postgreReconnect ()  {
-            postgreEvent();
+        function realTimeReconnect ()  {
+            realTimeEvent();
         }
     
-        function tsdbReconnect () {
-            tsdbEvent();
+        function agentInfoReconnect () {
+            agentInfoEvent();
         }
     
-        function elasticReconnect () {
-            elascitEvent();
-        }
-    
-
-        const postgreEvent = () => {
-
-            eventMemory1 = new EventSource("https://localhost:8443/event/postgres/usage");
-            if(eventMemory1.readyState !== eventMemory1.CLOSED) {
-                clearInterval(postgreInterval);
+       
+        const realTimeEvent = () => {
+            
+            eventMemory1 = new EventSource("https://127.0.0.1:8443/event/realtime/data");
+            if(eventMemory1.readyState !== eventMemory1.CLOSED) { 
+                clearInterval(realTimeInterval);
             }
 
             eventMemory1.onmessage = (event) => {
+                
+                setRealTimeData(event.data);
+                console.log("EventSource 1");
                 //    setData(event.data);
-                    const usagePs = JSON.parse(event.data);
-                    setmemoryUsagePs(usagePs.memoryUsage)
-                    setcpuUsagePs(usagePs.cpuUsage)
-                    setcurrentDatePs(usagePs.date)
-                    settaskTimePs(usagePs.taskTime)
+                // const usagePs = JSON.parse(event.data);
+                // setmemoryUsagePs(usagePs.memoryUsage)
+                // setcpuUsagePs(usagePs.cpuUsage)
+                // setcurrentDatePs(usagePs.date)
+                // settaskTimePs(usagePs.taskTime)
             }
             eventMemory1.onerror = (err) => {
                 console.error("EventSource Ps failed:", err);
                 eventMemory1.close();
-                postgreInterval = setInterval(() => {
-                    if(err.target.readyState === err.target.CLOSED) {
-                        postgreReconnect();
-                    }
-                }, 1000)
+                // realTimeInterval = setInterval(() => {
+                //     if(err.target.readyState === err.target.CLOSED) {
+                //         realTimeReconnect();
+                //     }
+                // }, 1000)
                 
             }
 
         }
     
-        const tsdbEvent = () => {
+        const agentInfoEvent = () => {
     
-            eventMemory2 = new EventSource("https://localhost:8443/event/time/usage");
+            eventMemory2 = new EventSource("https://127.0.0.1:8443/event/agentinfo/data");
             
             if(eventMemory2.readyState !== eventMemory2.CLOSED) {
-                clearInterval(tsdbInterval);
+                clearInterval(agentInfoInterval);
             }
 
             eventMemory2.onmessage = (event) => {
+                
                 //    setData(event.data);
-                    const usageTs = JSON.parse(event.data);
-                    setmemoryUsageTs(usageTs.memoryUsage)
-                    setcpuUsageTs(usageTs.cpuUsage)
-                    setcurrentDateTs(usageTs.date)
-                    settaskTimeTs(usageTs.taskTime)
+                setAgentData(event.data)
+                console.log("EventSource 22");
+                // const usageTs = JSON.parse(event.data);
+                
             }
             eventMemory2.onerror = (err) => {
                 console.error("EventSource Ts failed:", err);
                 eventMemory2.close();
-                tsdbInterval = setInterval(() => {
-                    if(err.target.readyState === err.target.CLOSED) {
-                        tsdbReconnect();
-                    } 
-                },1000)
+                // agentInfoInterval = setInterval(() => {
+                //     if(err.target.readyState === err.target.CLOSED) {
+                //         agentInfoReconnect();
+                //     } 
+                // },1000)
     
             }
     
         }
     
-        const elascitEvent = () => {
-    
-            eventMemory3 = new EventSource("https://localhost:8443/event/elastic/usage");
-            if(eventMemory3.readyState !== eventMemory3.CLOSED) {
-                clearInterval(elasticInterval);
-            }
-            eventMemory3.onmessage = (event) => {
-                //    setData(event.data);
-                    const usageEs = JSON.parse(event.data);
-                    setmemoryUsageEs(usageEs.memoryUsage)
-                    setcpuUsageEs(usageEs.cpuUsage)
-                    setcurrentDateEs(usageEs.date)
-                    settaskTimeEs(usageEs.taskTime)
-                }
-                eventMemory3.onerror = (err) => {
-                    console.error("EventSource Es failed:", err);
-                    eventMemory3.close();
-                    elasticInterval = setInterval(() => {
-                        if(err.target.readyState === err.target.CLOSED) {
-                            elasticReconnect();
-                            
-                        } 
-                    },1000)
-                   
-                }
-        }
+       
 
-        postgreEvent();
-        tsdbEvent();
-        elascitEvent();
+        realTimeEvent();
+        agentInfoEvent();
+
        
         return () => {
             eventMemory1.close();
             eventMemory2.close();
-            eventMemory3.close();
             console.log("event closed")
         }
 
@@ -144,10 +115,11 @@ export const Dashboard = () => {
 
   
     return (
-        <div style={{ "marginTop": "20px", "textAlign": "center", "height": "100%" }}>
-            <h3>Postagres Dashboard </h3>
-            <h5>조회 소요시간 : {taskTimePs}<br/>Ontune Time : {currentDatePs}</h5>
-            <div style={{ "display": "inline-flex" }}>
+        <div style={{ "marginTop": "20px", "textAlign": "center", "height": "100%", "width": "100%" }}>
+            <h3>Real Time Data Dashboard </h3>
+            <textarea value={realTimeData} style={{"fontSize":"12px", "height":"600px", "width": "90%","resize":"none"}}></textarea>
+            {/* <h5>조회 소요시간 : {taskTimePs}<br/>Ontune Time : {currentDatePs}</h5> */}
+            {/* <div style={{ "display": "inline-flex" }}>
                 <div style={{"marginRight":"50px"}}>
                     <ReactSpeedometer
                         maxValue={100}
@@ -171,11 +143,12 @@ export const Dashboard = () => {
                         textColor={"white"}
                     />
                 </div>
-            </div>
+            </div> */}
             <div>
-                <div style={{"float": "left", "margin":"100px"}}>
-                    <h3>TimeScale Dashboard</h3>
-                    <h5>조회 소요시간 : {taskTimeTs}<br/>Ontune Time : {currentDateTs}</h5>
+                <div style={{"margin":"50px"}}>
+                    <h3>AgentInfo Data</h3>
+                    <textarea value={agentData} style={{"fontSize":"12px", "height":"200px", "width": "90%","resize":"none"}}></textarea>
+                    {/* <h5>조회 소요시간 : {taskTimeTs}<br/>Ontune Time : {currentDateTs}</h5>
                     <div style={{ "display": "inline-flex" }}>
                         <div style={{"marginRight":"50px"}}>
                             <ReactSpeedometer
@@ -200,37 +173,7 @@ export const Dashboard = () => {
                                 textColor={"white"}
                             />
                         </div>
-                    </div>
-                </div>
-
-                <div style={{"float":"right", "margin":"100px"}}>
-                    <h3>Elastic Dashboard</h3>
-                    <h5>조회 소요시간 : {taskTimeEs}<br/>Ontune Time : {currentDateEs}</h5>
-                    <div style={{ "display": "inline-flex" }}>
-                        <div style={{"marginRight":"50px"}}>
-                            <ReactSpeedometer
-                                maxValue={100}
-                                value={cpuUsageEs}
-                                valueFormat={"d"}
-                                customSegmentStops={[0, 25, 50, 75, 100]}
-                                segmentColors={["#7788AA", "#4E648E", "#152B55", "#061639"]}
-                                currentValueText={"CPU Usage: ${value} %"}
-                                textColor={"white"}
-                            />
-                        </div>
-
-                        <div style={{"marginLeft":"50px"}}>
-                            <ReactSpeedometer
-                                maxValue={100}
-                                value={memoryUsageEs}
-                                valueFormat={"d"}
-                                customSegmentStops={[0, 25, 50, 75, 100]}
-                                segmentColors={["#C4C1CC", "#9791A7", "#5A5077", "#41375F"]}
-                                currentValueText={"Memory Usage: ${value} %"}
-                                textColor={"white"}
-                            />
-                        </div>
-                    </div>
+                    </div> */}
                 </div>
                 </div>
 
